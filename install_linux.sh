e64fd763fe37079aa302ee75dc9647f26ddfe8ca
Mateo de Mayo <mateodemayo@gmail.com>
2021-10-13 20:47:32 -0300

    Remove sudo from install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -2,17 +2,17 @@
 
 set -e  # makes the script fail as soon as one command fails
 
-sudo tlmgr install truncate
-sudo tlmgr install tocloft
-sudo tlmgr install wallpaper
-sudo tlmgr install morefloats
-sudo tlmgr install sectsty
-sudo tlmgr install siunitx
-sudo tlmgr install threeparttable
-sudo tlmgr update l3packages
-sudo tlmgr update l3kernel
-sudo tlmgr update l3experimental
-sudo tlmgr update l3backend
+tlmgr install truncate
+tlmgr install tocloft
+tlmgr install wallpaper
+tlmgr install morefloats
+tlmgr install sectsty
+tlmgr install siunitx
+tlmgr install threeparttable
+tlmgr update l3packages
+tlmgr update l3kernel
+tlmgr update l3experimental
+tlmgr update l3backend
 # Would it be simpler to just update all packages? (takes ~10m)
 # sudo tlmgr update --all
 pip install pandoc-fignos pandoc-eqnos pandoc-tablenos pandoc-secnos pandoc-shortcaption
